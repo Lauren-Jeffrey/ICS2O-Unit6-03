7edeb1ff8a66c2fa62c798e66887e51462454c9b
LaurenJeffrey <lauren.jeffrey@ocsbstudent.ca>
2020-12-18 14:02:53 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let age = prompt("How many years old are you?");
+if (age>=17) {
+  alert("You can watch an R rated movie alone.")
+}
+else if (age>=13) {
+  alert("You can watch a PG-13 rated movie alone.")
+}
+else if (age>=5) {
+  alert("You can watch a G or PG rated movie alone")
+}
+else {
+  alert("You're too young to watch most movies.")
+}
